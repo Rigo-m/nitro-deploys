fa33ee0b65c45d424a94bc1939bf8943bc17249a
Matteo Rigoni <matteo.rigoni@atoms.studio>
2023-10-14 11:44:16 +0200

    chore: testing headers without spaces

--- routes/api/hello.ts
+++ routes/api/hello.ts
@@ -1,12 +1,12 @@
 export default defineEventHandler((event) => {
   const query = getQuery(event);
 
-  setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
-  setHeader(
-    event,
-    "Netlify-CDN-Cache-Control",
-    "public,max-age=60,must-revalidate",
-  );
+  // setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
+  // setHeader(
+  //   event,
+  //   "Netlify-CDN-Cache-Control",
+  //   "public,max-age=60,must-revalidate",
+  // );
 
   return {
     api: "Works",
