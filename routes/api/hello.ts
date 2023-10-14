e09d544ccebdee27f079d15c6146191edf9d72fd
Matteo Rigoni <matteo.rigoni@atoms.studio>
2023-10-14 11:37:32 +0200

    chore: tested manually adding headers

--- routes/api/hello.ts
+++ routes/api/hello.ts
@@ -1,6 +1,13 @@
 export default defineEventHandler((event) => {
   const query = getQuery(event);
 
+  setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
+  setHeader(
+    event,
+    "Netlify-CDN-Cache-Control",
+    "public,max-age=60,must-revalidate",
+  );
+
   return {
     api: "Works",
     query,
