5e2ddf83cc23f9af90a01116628699d1a3103913
Matteo Rigoni <matteo.rigoni@atoms.studio>
2023-10-19 15:01:32 +0200

    chore: added timing

--- routes/api/hello.ts
+++ routes/api/hello.ts
@@ -1,6 +1,13 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const query = getQuery(event);
 
+  const sleep = async () => {
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(true), 5000);
+    });
+  };
+
+  await sleep();
   // setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
   // setHeader(
   //   event,
