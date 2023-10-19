8ece50a7b35d64a69897405b5af93374781c842e
Matteo Rigoni <matteo.rigoni@atoms.studio>
2023-10-19 15:07:47 +0200

    chore: changed route tests

--- routes/api/hello-swr.ts
+++ routes/api/hello-swr.ts
@@ -1,5 +1,13 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const query = getQuery(event);
+
+  const sleep = async () => {
+    new Promise((resolve) => {
+      setTimeout(() => resolve(true), 5000);
+    });
+  };
+
+  await sleep();
 
   return {
     api: "Works - should show previous cached response",
--- routes/api/hello.ts
+++ routes/api/hello.ts
@@ -2,18 +2,18 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event);
 
   const sleep = async () => {
-    await new Promise((resolve) => {
+    new Promise((resolve) => {
       setTimeout(() => resolve(true), 5000);
     });
   };
 
   await sleep();
-  // setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
-  // setHeader(
-  //   event,
-  //   "Netlify-CDN-Cache-Control",
-  //   "public,max-age=60,must-revalidate",
-  // );
+  setHeader(event, "Cache-Control", "public,max-age=0,must-revalidate");
+  setHeader(
+    event,
+    "Netlify-CDN-Cache-Control",
+    "public,max-age=3600,must-revalidate",
+  );
 
   return {
     api: "Works",
